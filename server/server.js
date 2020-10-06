037afd465c9f3629dec18f0b1203a4cd47635f44
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-10-06 22:26:54 +0200

    added server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname, '/../build')));
 
 app.use((req, res, next) => {
-	console.log(`${req.method} ${req.url}`);
+	console.log(`${req.method} ${req.url}`); 
 	next()
 })
 
