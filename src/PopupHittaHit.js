ca9248318ef5e8d5ca68f62923d78523c3739cd2
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-10-07 01:09:50 +0200

    css

--- src/PopupHittaHit.js
+++ src/PopupHittaHit.js
@@ -11,9 +11,9 @@ const [show, setShow] = useState(false);
       <div className="popUpModal" onClick={() => setShow(show => !show)}>
         <div className="popUpDiv">
 		<div className="popUpX" onClick={() => setShow(show => show)}> x </div>
-		<br/> <div className="mapText">Kliniken är belägen inne i köpcentret Nordstan på 4e våningen. <br/> Hiss och entre finnes mellan Glasskiosken och Synoptik (mittemot Telenor). <br/><br/>
+		{/* <br/> <div className="mapText">Kliniken är belägen inne i köpcentret Nordstan på 4e våningen. <br/> Hiss och entre finnes mellan Glasskiosken och Synoptik (mittemot Telenor). <br/><br/>
 		  <img src={map} className="mapPic" alt="map" />
-		  </div>
+		  </div> */}
 		  </div>
         </div>
 	</FadeEffect> 
