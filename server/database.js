06978eb0fd7b693414a7c2d35b8b4d4677303508
alexandrajampolskaia <a.jampolskaia@gmail.com>
2021-05-21 17:18:20 +0200

    commented out server/database

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
-const { MongoClient, ObjectID } = require("mongodb");
-const url =
-  "mongodb+srv://alex:jamp@cluster0.tbdjh.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const { MongoClient, ObjectID } = require("mongodb");
+// const url =
+//   "mongodb+srv://alex:jamp@cluster0.tbdjh.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-const dbName = "boats";
-const collectionName = "boats";
+// const dbName = "boats";
+// const collectionName = "boats";
 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-//server.js
-const express = require('express'); 
-const app = express(); 
-const port = process.env.PORT || 1238;
-const path = require('path')
-const bodyParser = require('body-parser');
+// //server.js
+// const express = require('express'); 
+// const app = express(); 
+// const port = process.env.PORT || 1238;
+// const path = require('path')
+// const bodyParser = require('body-parser');
 
-//MIDDLEWARE
-app.use(express.static(path.join(__dirname, '/../build')));
+// //MIDDLEWARE
+// app.use(express.static(path.join(__dirname, '/../build')));
 
-app.use((req, res, next) => {
-	console.log(`${req.method} ${req.url}`); 
-	next()
-})
+// app.use((req, res, next) => {
+// 	console.log(`${req.method} ${req.url}`); 
+// 	next()
+// })
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json());
 
 
-//Start listening to the server 
-app.listen(port, console.log("Running port", `${port}`));
+// //Start listening to the server 
+// app.listen(port, console.log("Running port", `${port}`));
